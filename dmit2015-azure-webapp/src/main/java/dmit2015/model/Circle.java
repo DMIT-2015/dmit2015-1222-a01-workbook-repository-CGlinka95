318e26a21ce43836e07ec0a6430ba1d388d66d85
CGlinka95 <glinkachris@gmail.com>
2023-01-13 11:54:39 -0700

    Created a circle class to demonstrate the program at runtime.

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/Circle.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/Circle.java
@@ -43,4 +43,29 @@ public class Circle {
     {
         return 2 * Math.PI * radius;
     }
+
+    public static void main (String[] args)
+    {
+        Circle circle1 = new Circle();
+
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.Area());
+        System.out.printf("The perimeter of circle1 is %.2f\n", circle1.Perimeter());
+
+        circle1.setRadius(5);
+
+        System.out.printf("The radius of circle1 is %s\n", circle1.getRadius());
+        System.out.printf("The area of circle1 is %.5f\n", circle1.Area());
+        System.out.printf("The perimeter of circle1 is %.2f\n", circle1.Perimeter());
+
+        try
+        {
+            circle1.setRadius(-25);
+            System.out.println("An exception should have been thrown.");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
