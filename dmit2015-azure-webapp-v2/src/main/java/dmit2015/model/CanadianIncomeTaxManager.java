8f52ac465f4076d15a7714c8e94f5cdcdf7333a2
CGlinka95 <glinkachris@gmail.com>
2023-02-03 13:01:31 -0700

    Updated CanadianIncomeTaxManager class and CanadianPersonalIncomeTaxRate classes to match what was done today in class.

--- dmit2015-azure-webapp-v2/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
+++ dmit2015-azure-webapp-v2/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
@@ -1,13 +1,18 @@
 package dmit2015.model;
 
+import com.beust.ah.A;
 import lombok.Getter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class CanadianIncomeTaxManager {
     //Define a private constructor to implement single pattern
@@ -29,16 +34,35 @@ public class CanadianIncomeTaxManager {
     @Getter
     private List<CanadianPersonalIncomeTaxRate> incomeTaxRates;
 
+    public int[] availableTaxYears() {
+        return incomeTaxRates.stream()
+                .map(item -> item.getTaxYear())
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(item -> item)
+                .toArray();
+    }
+
     public void loadDataFromFile() {
-        List<String> incomeTaxData = new ArrayList<>();
         try {
-            Path csvPath = Path.of(
-                    getClass()
-                            .getClassLoader()
-                            .getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
-            //incomeTaxRates = Files.readAllLines(csvPath);
-        } catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(e);
+            try (var reader = new BufferedReader(new InputStreamReader(
+                    getClass().getResourceAsStream("/data/csv/CanadianPersonalIncomeTaxRates.csv")))) {
+                // Skip the first line as it contains headers
+                reader.readLine();
+                String line;
+                incomeTaxRates = new ArrayList<>();
+                while ( (line = reader.readLine()) != null) {
+                    var optionalCanadianPersonalIncomeTaxRate = CanadianPersonalIncomeTaxRate.parseCsv(line);
+                    if (optionalCanadianPersonalIncomeTaxRate.isPresent()) {
+                        CanadianPersonalIncomeTaxRate currentCanadianPersonalIncomeTaxRate
+                                = optionalCanadianPersonalIncomeTaxRate.orElseThrow();
+                        incomeTaxRates.add(currentCanadianPersonalIncomeTaxRate);
+                    }
+                }
+
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
--- dmit2015-azure-webapp-v2/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRate.java
+++ dmit2015-azure-webapp-v2/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRate.java
@@ -45,27 +45,31 @@ public class CanadianPersonalIncomeTaxRate {
         if (tokens.length != 9) {
             throw new RuntimeException("Line does not contain exactly 9 values!");
         }
-        String regionAbbrev = tokens[0];
-        String regionName = tokens[1];
-        int taxYear = Integer.parseInt(tokens[2]);
-        int bracketNo = Integer.parseInt(tokens[3]);
-        double startingIncome = Double.parseDouble(tokens[4]);
-        // Double endingIncome = tokens[5].isBlank() ? null : Double.valueOf(tokens[5]);
-        Double endingIncome = tokens[5].isBlank() ? Integer.MAX_VALUE : Double.valueOf(tokens[5]);
-        double taxRate = Double.parseDouble(tokens[6]);
-        double baseTax = Double.parseDouble(tokens[7]);
-        CanadianPersonalIncomeTaxRate currentIncomeTaxRate = new CanadianPersonalIncomeTaxRate();
-        currentIncomeTaxRate.setRegionAbbreviation(regionAbbrev);
-        currentIncomeTaxRate.setRegionName(regionName);
-        currentIncomeTaxRate.setTaxYear(taxYear);
-        currentIncomeTaxRate.setBracketNo(bracketNo);
-        currentIncomeTaxRate.setStartingIncome(startingIncome);
-        currentIncomeTaxRate.setEndingIncome(endingIncome);
-        currentIncomeTaxRate.setTaxRate(taxRate);
-        currentIncomeTaxRate.setBaseTaxAmount(baseTax);
+        try {
+            String regionAbbrev = tokens[0];
+            String regionName = tokens[1];
+            int taxYear = Integer.parseInt(tokens[2]);
+            int bracketNo = Integer.parseInt(tokens[3]);
+            double startingIncome = Double.parseDouble(tokens[4]);
+            Double endingIncome = tokens[5].isBlank() ? null : Double.valueOf(tokens[5]);
+//        Double endingIncome = tokens[5].isBlank() ? Integer.MAX_VALUE : Double.valueOf(tokens[5]);
+            double taxRate = Double.parseDouble(tokens[6]);
+            double baseTax = Double.parseDouble(tokens[7]);
+            CanadianPersonalIncomeTaxRate currentIncomeTaxRate = new CanadianPersonalIncomeTaxRate();
+            currentIncomeTaxRate.setRegionAbbreviation(regionAbbrev);
+            currentIncomeTaxRate.setRegionName(regionName);
+            currentIncomeTaxRate.setStartingIncome(startingIncome);
+            currentIncomeTaxRate.setEndingIncome(endingIncome);
+            currentIncomeTaxRate.setTaxRate(taxRate);
+            currentIncomeTaxRate.setTaxYear(taxYear);
+            currentIncomeTaxRate.setBracketNo(bracketNo);
+            currentIncomeTaxRate.setBaseTaxAmount(baseTax);
 
-        optionalCanadianPersonalIncomeTaxRate = Optional.of(currentIncomeTaxRate);
-
+            optionalCanadianPersonalIncomeTaxRate = Optional.of(currentIncomeTaxRate);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
         return optionalCanadianPersonalIncomeTaxRate;
     }
 }
