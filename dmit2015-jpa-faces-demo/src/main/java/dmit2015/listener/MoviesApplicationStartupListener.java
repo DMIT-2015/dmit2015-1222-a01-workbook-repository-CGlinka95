2c0e8533e3c12d33591bbda7e2426cb613f06736
CGlinka95 <glinkachris@gmail.com>
2023-02-21 13:57:38 -0700

    Catching up on previous lessons and reviewing topics before starting assignment 03.

---
+++ dmit2015-jpa-faces-demo/src/main/java/dmit2015/listener/MoviesApplicationStartupListener.java
@@ -0,0 +1,80 @@
+package dmit2015.listener;
+
+import dmit2015.entity.Movie;
+import dmit2015.persistence.MovieRepository;
+import jakarta.inject.Inject;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Optional;
+
+@WebListener
+public class MoviesApplicationStartupListener implements ServletContextListener, HttpSessionListener, HttpSessionAttributeListener {
+
+    @Inject
+    MovieRepository _movieRepository;
+
+    public MoviesApplicationStartupListener() {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        /* This method is called when the servlet context is initialized(when the Web application is deployed). */
+
+        if (_movieRepository.count() == 0) {
+            try {
+                try (var reader = new BufferedReader(new InputStreamReader(
+                        Objects.requireNonNull(getClass().getResourceAsStream("/data/csv/movies.csv")))) ) {
+                    String line;
+                    final var delimiter = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+                    // Skip the first line as it is containing column headings
+                    reader.readLine();
+                    while ((line = reader.readLine()) != null) {
+                        Optional<Movie> optionalMovie = Movie.parseCsv(line);
+                        if (optionalMovie.isPresent()) {
+                            Movie csvMovie = optionalMovie.orElseThrow();
+                            _movieRepository.add(csvMovie);
+                        }
+                    }
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        /* This method is called when the servlet Context is undeployed or Application Server shuts down. */
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        /* Session is created. */
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        /* Session is destroyed. */
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+        /* This method is called when an attribute is added to a session. */
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+        /* This method is called when an attribute is removed from a session. */
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+        /* This method is called when an attribute is replaced in a session. */
+    }
+}
---
+++ dmit2015-jpa-javase-demo/src/main/java/ca/nait/dmit/Main.java
@@ -0,0 +1,7 @@
+package ca.nait.dmit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
